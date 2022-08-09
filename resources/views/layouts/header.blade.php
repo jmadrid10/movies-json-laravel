943187bbe909c04bfed23b570c1d864703025e75
jmadrid10 <jesus.madrid1911@gmail.com>
2022-08-09 18:04:45 -0400

    Modificando titulo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container">
-      <a class="navbar-brand" href="#">Movies</a>
+      <a class="navbar-brand" href="#">Movies JSON</a>
     </div>
 </nav>
